33a59030dce2ce38daed2ed74e58590189822c36
Mykola Malovanchuk <113469067+mykolazx3@users.noreply.github.com>
2022-10-21 19:06:17 +0300

    Add files via upload

---
+++ src/main/java/com/example/demoprodjecttestdeploy/DemoProdjectTestDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoprodjecttestdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProdjectTestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProdjectTestDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoprodjecttestdeploy/DemoProdjectTestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoprodjecttestdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProdjectTestDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
